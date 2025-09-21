c944b81e25baa7c6056928448d22ca9a8be10fb6
Ho Tsun Lok <tsunlokho2015@gmail.com>
2025-09-22 00:38:57 +0100

    Update thermostat.vhd

--- lab1-basic-multiplexer/thermostat.vhd
+++ lab1-basic-multiplexer/thermostat.vhd
@@ -1,1 +1,18 @@
+entity THERMOSTAT is
+  port ( CURRENT_TEMP : in bit_vector(6 downto 0);
+         DESIRED_TEMP : in bit_vector(6 downto 0);
+         DISPLAY_SELECT : in bit;
+         TEMP_DISPLAY : out bit_vector(6 downto 0));
+end THERMOSTAT;
+architecture BEHAV of THERMOSTAT is
+begin
 
+process (CURRENT_TEMP,DESIRED_TEMP,DISPLAY_SELECT)
+begin
+if DISPLAY_SELECT = '1' then
+TEMP_DISPLAY <= CURRENT_TEMP;
+else
+TEMP_DISPLAY <= DESIRED_TEMP;
+end if;
+end process;
+end BEHAV;
