f9214cf05625fe548eea8540fb98e71bd430e72d
Ho Tsun Lok <tsunlokho2015@gmail.com>
2025-10-06 19:44:09 +0100

    Create math_package.vhd

---
+++ lab5-addcounter/math_package.vhd
@@ -0,0 +1,7 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package math_package is
+    function AC_COUNTDOWN(curr_value : integer) return integer;
+    function FN_COUNTDOWN(curr_value : integer) return integer;
+end package math_package;
